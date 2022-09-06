fb124ff32b2c10499c27f19851621bbf2f1271f5
Emmanuel Flores <flores.emmanuel@gmail.com>
2022-09-06 03:20:06 -0300

    Header creado
    Header y logo de Argentina Programa

--- Portfolio_EmmanuelFlores/src/app/app.module.ts
+++ Portfolio_EmmanuelFlores/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ApLogoComponent } from './components/ap-logo/ap-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ApLogoComponent
   ],
   imports: [
     BrowserModule,
