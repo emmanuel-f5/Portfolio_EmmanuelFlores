64460d35811de17c6f1805b228f1c72a68a3cdd8
Emmanuel Flores <flores.emmanuel@gmail.com>
2022-09-19 02:50:43 -0300

    Foto de perfil y Acerca De
    Se agregaron fotos de perfil, nombre completo y título, y "acerca de".

--- Portfolio_EmmanuelFlores/src/app/app.module.ts
+++ Portfolio_EmmanuelFlores/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ApLogoComponent } from './components/ap-logo/ap-logo.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     ApLogoComponent,
     ContactoComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
