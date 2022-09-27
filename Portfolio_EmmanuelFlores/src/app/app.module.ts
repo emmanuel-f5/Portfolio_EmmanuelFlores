5a045d64783f1976f73d535f78025bdd355ae217
Emmanuel Flores <flores.emmanuel@gmail.com>
2022-09-27 05:28:13 -0300

    Experiencia y Educación
    Se terminó de darle estilo general al componente "Acerca de" y se agregaron los componentes de Experiencia Laboral y Educación, con sus estilos.

--- Portfolio_EmmanuelFlores/src/app/app.module.ts
+++ Portfolio_EmmanuelFlores/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ApLogoComponent } from './components/ap-logo/ap-logo.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     ApLogoComponent,
     ContactoComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
